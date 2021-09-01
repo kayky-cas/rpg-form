11702ebe1f46318d1388abc64f1f903c15540208
Kayky Casagrande <kayky-cas@github.com>
2021-09-01 02:20:29 +0000

    App home

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
 
   await app.listen(3000);
 }
